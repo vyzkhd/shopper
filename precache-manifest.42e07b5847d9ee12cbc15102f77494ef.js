6cf15161a0f7cc5995972b328a2825cc10ad132f
vyzkhdb <vysh2010@gmail.com>
2020-05-23 14:31:56 +0530

    Updates

---
+++ precache-manifest.42e07b5847d9ee12cbc15102f77494ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a844d15503062d167008dd7b582a290",
+    "url": "/shopper/index.html"
+  },
+  {
+    "revision": "95bf14a52a5721ad8151",
+    "url": "/shopper/static/css/main.1c0c3b32.chunk.css"
+  },
+  {
+    "revision": "dbc4ef45361e3868fa2c",
+    "url": "/shopper/static/js/2.2d695d9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopper/static/js/2.2d695d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95bf14a52a5721ad8151",
+    "url": "/shopper/static/js/main.1f36812d.chunk.js"
+  },
+  {
+    "revision": "e7f92c89057668ca6325",
+    "url": "/shopper/static/js/runtime-main.6991b1f2.js"
+  }
+]);
